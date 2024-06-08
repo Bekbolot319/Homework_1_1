df15d95b1c950032fa95b9dc89c9a745de5c068a
Bekbolot319 <beksattarob42@gmail.com>
2024-06-08 20:12:14 +0600

    g

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String myName;
+        final int NUM = 13;
+        String word = "Peace";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        myName = NUM + word;
+        System.out.println(NUM);
+        System.out.println(myName);
+        System.out.println(word);
+
+        if (NUM < 0) {
+            System.out.println("Number is negative");
+        } else if (NUM > 0) {
+            System.out.println("Number is positive");
+        } else {
+            System.out.println("Number is zero");
         }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name ");
+
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name + "!");
+
     }
 }
